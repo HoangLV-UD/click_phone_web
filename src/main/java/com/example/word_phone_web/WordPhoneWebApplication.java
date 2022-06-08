66df0c1274400cf7dc1f15645105b6ca983ae1da
hieu230201 <hieuop130316@gmail.com>
2022-06-08 20:01:02 +0700

    first commit

---
+++ src/main/java/com/example/word_phone_web/WordPhoneWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.word_phone_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordPhoneWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordPhoneWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/word_phone_web/WordPhoneWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.word_phone_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordPhoneWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
